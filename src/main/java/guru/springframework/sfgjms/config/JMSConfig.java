344ab9d022fc7a058d7a2af15bae2edca1ad02ea
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-11 01:00:59 +0100

    Adding TaskConfig and JMSConfig

---
+++ src/main/java/guru/springframework/sfgjms/config/JMSConfig.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgjms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JMSConfig {
+
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+}
